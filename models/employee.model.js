76dd51bbfea2c236cb29055c284ce4225c586e44
gavin vaske <gavin@vantacollective.com>
2020-09-02 20:06:23 -0500

    Updated employee schema

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
+let validateEmail = function(email) {
+  let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
 const employeeSchema = new mongoose.Schema({
   fullName: {
-    type: String
+    type: String,
+    required: 'This field is required'
   },
   email: {
-    type: String
+    type: String,
+    required: 'Email address is required',
+    validate: [validateEmail, 'Please enter a valid email address']
   },
   mobile: {
     type: String
