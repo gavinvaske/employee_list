30a956fa3794e6a66a6f132ef5e71e5c30398c38
gavin vaske <gavin@vantacollective.com>
2020-09-02 20:05:46 -0500

    Added body parser

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const express = require('express');
 const employeeController = require('./controllers/employeeController');
 const expressHandlebars = require('express-handlebars');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  }));
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', expressHandlebars({extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('view engine', 'hbs');
@@ -20,9 +25,6 @@ app.listen(process.env.PORT, () => {
 });
 
 app.use('/employee', employeeController);
-
-
-
 
 
 console.log(process.env.DB_URL);
